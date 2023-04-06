cd49d9159de750faddf977f3021f43c4a709e8d1
Michael Orenstein <michael@whiteshadows.me>
2023-04-06 14:05:23 +0930

    Fixed default export

--- BumpRig/src/application/pages/DevicePages/ConfigurationPage.tsx
+++ BumpRig/src/application/pages/DevicePages/ConfigurationPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
-const ConfigurationPage = (props: RouteComponentProps) => {
+
+export const ConfigurationPage = (props: RouteComponentProps) => {
   return <React.Fragment>This page smells like a new car...</React.Fragment>
 }
-export default ConfigurationPage
+ 
